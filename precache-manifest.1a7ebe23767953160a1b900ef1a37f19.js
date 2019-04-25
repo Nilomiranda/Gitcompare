408472f9b478d9bab41648126c9f8172d3276f5d
Nilomiranda <nilomiranda3@gmail.com>
2019-04-24 23:03:51 -0300

    Updates

---
+++ precache-manifest.1a7ebe23767953160a1b900ef1a37f19.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gitcompare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gitcompare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gitcompare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gitcompare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gitcompare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "dd9035d1acb93032aa19",
+    "url": "/gitcompare/static/js/runtime~main.8455338b.js"
+  },
+  {
+    "revision": "ac14904806f496a92598",
+    "url": "/gitcompare/static/js/main.b3493fa9.chunk.js"
+  },
+  {
+    "revision": "b623dbca3959ceada904",
+    "url": "/gitcompare/static/js/2.3376abd1.chunk.js"
+  },
+  {
+    "revision": "b623dbca3959ceada904",
+    "url": "/gitcompare/static/css/2.0223d4ec.chunk.css"
+  },
+  {
+    "revision": "76c1b412324bc01857865b842157d925",
+    "url": "/gitcompare/index.html"
+  }
+];
